fbd9991172f9846f2fcc1062c84b8147d5b40864
MinJun Kweon <Minz0000000@gmail.com>
2015-11-27 01:55:37 +0900

    add logger (morgan npm)

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ var express = require('express');
 var user = require('./models/user');
 var login = require('./controllers/login');
 var app = express();
+var logger = require('morgan');
+
+if (!module.parent) {
+    app.use(logger('dev'));
+}
 
 app.get('/', function(req, res){
     res.send('Hello World');
