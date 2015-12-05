7280ed34a72d54ae56ddc89a2239cd0240257c56
MinJun Kweon <Minz0000000@gmail.com>
2015-12-05 18:44:20 +0900

    hotfix: remove the elements removed primary key

--- script/LectureOutlineMaker.js
+++ script/LectureOutlineMaker.js
@@ -8,9 +8,10 @@ var lectureOutlines = [];
 xlrd.open('2015-2.xls', function(err, book) {
 		sheet = book.sheet.byIndex(0);
 		for (var i = 1; i < sheet.row.count; ++i) {
-			l                     = {};
-			l[ 'curriculum' ]     = sheet.cell.getRaw(i, 1);
-			l[ 'lecture_id' ]     = sheet.cell.getRaw(i, 3);
+			l                 = {};
+			l[ 'curriculum' ] = sheet.cell.getRaw(i, 1);
+			l[ 'lecture_id' ] = sheet.cell.getRaw(i, 3);
+			if (l[ 'lecture_id' ] == 0) continue;
 			l[ 'title' ]          = sheet.cell.getRaw(i, 4);
 			l[ 'professor_name' ] = sheet.cell.getRaw(i, 5);
 			l[ 'point' ]          = sheet.cell.getRaw(i, 9);
@@ -18,7 +19,7 @@ xlrd.open('2015-2.xls', function(err, book) {
 		}
 		fs.writeFile('./2015-2.json', JSON.stringify(lectureOutlines), function(err) {
 			if(err) throw err;
-			console.log('File write completed');
+			console.log('File write completed : %d elements', lectureOutlines.length);
 		});
 	}
 );
