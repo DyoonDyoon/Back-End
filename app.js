ec4c157670a63046ef0d99c71339b5d7e254d057
MinJun Kweon <Minz0000000@gmail.com>
2015-12-05 23:29:03 +0900

    hotfix: didn't refresh the lecture outline config

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var app = express();
 var logger = require('morgan');
 var passportConfig = require('./controllers/passport');
 var target = require('./target');
+var fs = require('fs');
 
 if (!module.parent) {
     app.use(logger('dev'));
@@ -19,6 +20,16 @@ app.use(passport.session());
 
 app.get('/', function(req, res){
     res.send('Hello World');
+});
+
+app.get('/lectureOutline', function(req, res) {
+	fs.readFile('./config/lectureConfig.json', 'utf8', function (err, data) {
+		if (err) {
+			res.status(500).json({"message" : "CANNOT find lecture outline information"});
+			return;
+		}
+		res.json(JSON.parse(data));
+	});
 });
 
 app.post('/login', passportConfig.logIn);
