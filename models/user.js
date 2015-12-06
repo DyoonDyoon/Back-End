793ba00800ce14130c69401da0d3f7a7e12f4fe1
MinJun Kweon <Minz0000000@gmail.com>
2015-12-07 03:37:07 +0900

    control conflict

--- models/user.js
+++ models/user.js
@@ -7,11 +7,37 @@ var connectionPool = mysql.createPool(dbConfig);
 
 exports.list = function (req, res) {
 	connectionPool.getConnection(function (err, connection) {
-		connection.query('SELECT * FROM user', function (err, rows, fields) {
-			if (err) throw err;
+    connection.query('SELECT * FROM user', function (err, rows, fields) {
+      if (err) throw err;
+	    connection.release();
+      return res.json(rows);
+    });
+  });
+};
 
-			connection.release();
-			return res.json(rows);
+/*************************************
+ * the attributes which could be
+ *
+ * password - SHA1
+ * major -
+ * name -
+ *************************************/
+exports.update = function (req, res, next) {
+	connectionPool.getConnection(function (err, connection) {
+		var updateSql = 'UPDATE user SET ? WHERE uid = ?';
+		var updateParams = [];
+		if (req.query['password']) {
+
+		}
+		if (req.query['major']) {
+
+		}
+		if (req.query['name']) {
+
+		}
+
+		connection.query(updateSql, updateParams, function (err, rows, fields) {
+
 		});
-	});
-}
+	})
+};
